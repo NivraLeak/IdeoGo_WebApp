42923873aeb1b8967d1f40bd8ad71d3a830d469a
erick <erickquiber@gmail.com>
2020-06-25 21:28:59 -0500

    delete Bar

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,7 @@
       <v-toolbar-title ccentered>IdeoGo</v-toolbar-title>
     </v-app-bar>
     <v-content>
-      <v-tabs v-model="tab" centered>
-        <v-tab>Home</v-tab>
-        <v-tab>About</v-tab>
-        <v-tab>Contact Us</v-tab>
-        <v-tab>Premium</v-tab>
-        <v-tab>Login</v-tab>
-        <v-tab></v-tab>
-
-      </v-tabs>
+      
 
     </v-content>
   </v-app>
