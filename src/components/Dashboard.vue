f41b2b814593555674d49f23889cdea54d976440
erick <erickquiber@gmail.com>
2020-06-27 06:52:00 -0500

    project

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,55 +1,59 @@
 <template>
     <div class="col-md-12">
         <div class="card card-container">
-        <v-app id="style-1">
-            <v-card>
-                <v-navigation-drawer permanent fixed app clipped-left width="300px" >
-                    <v-list-item>
-                        <v-list-item-avatar>
-                            <img src="https://randomuser.me/api/portraits/men/81.jpg">
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                            <v-list-item-title>Luis Canaval</v-list-item-title>
-                            <v-list-item-subtitle>Gerente</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <v-tabs vertical v-model="tab" class="title1">
-                        <v-tab :class="title1" @click="setSelected('Dashboard')">
-                            <v-icon left size="25">dashboard</v-icon>
-                            <v-list-item-title class="hidden">Dashboard</v-list-item-title>
-                        </v-tab>
-                        <v-tab :class="title2" @click="setSelected('Projects')">
-                            <v-icon left size="25">business_center</v-icon>
-                            <v-list-item-title class="hidden">My Internships</v-list-item-title>
-                        </v-tab>
-                        <v-tab :class="title3" @click="setSelected('CreateProject')">
-                            <v-icon left size="25">add</v-icon>
-                            <v-list-item-title class="hidden">Create Internship</v-list-item-title>
-                        </v-tab>
-                        <v-tab :class="title4" @click="setSelected('Edit_Profile')">
-                            <v-icon left size="25">account_circle</v-icon>
-                            <v-list-item-title class="hidden">Edit Profile</v-list-item-title>
-                        </v-tab>
-                    </v-tabs>
-                    <template v-slot:append>
-                        <div class="pa-2">
-                            <v-btn href="/" block>Logout</v-btn>
-                        </div>
-                    </template>
-                </v-navigation-drawer>
-            </v-card>
-            <v-main>
-                <v-tabs-items v-model="tab">
-                    <v-tab-item>
-                        <CreateProject></CreateProject>
-                    </v-tab-item>
-                    <v-tab-item>
-                        <EditProfile></EditProfile>
-                    </v-tab-item>
-                </v-tabs-items>
-            </v-main>
-        </v-app>
-    </div>
+            <v-app id="style-1">
+                <v-card>
+                    <v-navigation-drawer permanent fixed app clipped-left width="300px">
+                        <v-list-item>
+                            <v-list-item-avatar>
+                                <img src="https://randomuser.me/api/portraits/men/81.jpg">
+                            </v-list-item-avatar>
+                            <v-list-item-content>
+                                <v-list-item-title>Luis Canaval</v-list-item-title>
+                                <v-list-item-subtitle>Gerente</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-tabs vertical v-model="tab" class="title1">
+                            <v-tab :class="title1" @click="setSelected('Dashboard')">
+                                <v-icon left size="25">dashboard</v-icon>
+                                <v-list-item-title class="hidden">Dashboard</v-list-item-title>
+                            </v-tab>
+                            <v-tab :class="title2" @click="setSelected('Projects')">
+                                <v-icon left size="25">business_center</v-icon>
+                                <v-list-item-title class="hidden">My Internships</v-list-item-title>
+                            </v-tab>
+                            <v-tab :class="title3" @click="setSelected('CreateProject')">
+                                <v-icon left size="25">add</v-icon>
+                                <v-list-item-title class="hidden">Create Internship</v-list-item-title>
+                            </v-tab>
+                            <v-tab :class="title4" @click="setSelected('EditProfile')">
+                                <v-icon left size="25">account_circle</v-icon>
+                                <v-list-item-title class="hidden">Edit Profile</v-list-item-title>
+                            </v-tab>
+                            <v-tab :class="title5" @click="setSelected('project')">
+                                <v-icon left size="25">account_circle</v-icon>
+                                <v-list-item-title class="hidden">Project</v-list-item-title>
+                            </v-tab>
+                        </v-tabs>
+                        <template v-slot:append>
+                            <div class="pa-2">
+                                <v-btn href="/" block>Logout</v-btn>
+                            </div>
+                        </template>
+                    </v-navigation-drawer>
+                </v-card>
+                <v-main>
+                    <v-tabs-items v-model="tab">
+                        <v-tab-item>
+                            <CreateProject></CreateProject>
+                        </v-tab-item>
+                        <v-tab-item>
+                            <EditProfile></EditProfile>
+                        </v-tab-item>
+                    </v-tabs-items>
+                </v-main>
+            </v-app>
+        </div>
     </div>
 </template>
 
@@ -57,6 +61,7 @@
     // import axios from 'axios'
     import EditProfile from "./EditProfile";
     import CreateProject from "./CreateProject";
+    import Project from "../view/Project";
     // import FindProject from "@/components/FindProject";
 
     export default {
@@ -64,7 +69,8 @@
         components: {
             //FindProject,
             CreateProject,
-            EditProfile
+            EditProfile,
+            Project,
         },
         props: {},
         data: () => ({
@@ -74,6 +80,7 @@
             title2: 'title',
             title3: 'title',
             title4: 'title',
+            title5: 'title',
             errors: [],
             tab: null,
             isHidden: true,
@@ -96,6 +103,13 @@
                     this.title2 = "title";
                     this.title3 = "title";
                     this.title4 = "title-selected";
+                    this.title5 = "title";
+                }
+                if (info === 'Project') {
+                    this.title1 = "title";
+                    this.title2 = "title";
+                    this.title3 = "title";
+                    this.title5 = "title-selected";
                 }
                 return this.selected;
             },
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="app">
-        <nav class="navbar navbar-expand navbar-dark bg-dark">
-            <a href class="navbar-brand" @click.prevent>Ideo GO</a>
+        <nav class="navbar navbar-expand navbar-dark bg-dark ">
+
             <div class="navbar-nav mr-auto">
                 <li class="nav-item"><router-link to="/home" class="nav-link"><font-awesome-icon icon="home"/> Home</router-link></li>
                 <li v-if="currentUser" class="nav-item"><router-link to="/users" class="nav-link">Users</router-link></li>
             </div>
+            <di class="navbar-nav ml-auto">
+                <h1 href class="navbar-brand" @click.prevent >Ideo GO</h1>
+            </di>
             <div v-if="!currentUser" class="navbar-nav ml-auto">
                 <li class="nav-item"><router-link to="/register" class="nav-link"><font-awesome-icon icon="user-plus"/> Sign Up</router-link></li>
                 <li class="nav-item"><router-link to="/login" class="nav-link"><font-awesome-icon icon="sign-in-alt"/> Sign In</router-link></li>
@@ -17,14 +20,58 @@
         </nav>
         <div class="container">
 
+
         </div>
+        <v-footer
+                dark
+                padless
+                class="footer"
+        >
+            <v-card
+                    flat
+                    tile
+                    class="indigo lighten-1 white--text text-center"
+            >
+                <v-card-text>
+                    <v-btn
+                            v-for="icon in icons"
+                            :key="icon"
+                            class="mx-4 white--text"
+                            icon
+                    >
+                        <v-icon size="24px">{{ icon }}</v-icon>
+                    </v-btn>
+                </v-card-text>
+
+                <v-card-text class="white--text pt-0">
+                    LA MEJOR PLATAFORMA PARA PODER SUBIR TUS PROYECTOS
+                    Nosotros queremos que tus proyectos logren ser sostenibles y exitosos &
+                    Ser tu primera opcion es nuestra finalidad, por ello nuestro modelo de internacionalización es un modelo inclusivo para responder a las necesidades locales.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-text class="white--text">
+                    {{ new Date().getFullYear() }} — <strong>IdeoGo</strong>
+                </v-card-text>
+            </v-card>
+        </v-footer>
     </div>
 </template>
 
 <script>
 
+
     export default {
         name: 'Home',
+        data: () => ({
+            icons: [
+                'mdi-facebook',
+                'mdi-twitter',
+                'mdi-linkedin',
+                'mdi-instagram',
+            ],
+        }),
 
         computed: {
             currentUser() {
@@ -42,5 +89,11 @@
 
 
 <style scoped>
+    .footer{
+        position: fixed;
+        bottom: 0;
+
+    }
+
 
 </style>
