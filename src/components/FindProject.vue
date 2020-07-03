49ab5ab7084775240b2da9aee206f698c944741f
nivra <nivraleak@outlook.com>
2020-07-02 19:42:17 -0500

    Eliminando Buscador

--- src/components/FindProject.vue
+++ src/components/FindProject.vue
@@ -5,40 +5,7 @@
             <div class="col">
                 <h4 class="text-center ">Ultimos proyectos publicados</h4>
                 <div class="row">
-                    <div id="FindProject">
-                        <v-app id="inspire">
-                            <v-card>
-                                <v-card-title>
-                                    Nutrition
-                                    <v-spacer></v-spacer>
-                                    <v-text-field
-                                            v-model="search"
-                                            append-icon="search"
-                                            label="Search"
-                                            single-line
-                                            hide-details
-                                    ></v-text-field>
-                                </v-card-title>
-                                <v-data-table
-                                        :headers="headers"
-                                        :items="desserts"
-                                        :search="search"
-                                >
-                                    <template slot="items" slot-scope="props">
-                                        <td>{{ props.item.name }}</td>
-                                        <td class="text-xs-right">{{ props.item.calories }}</td>
-                                        <td class="text-xs-right">{{ props.item.fat }}</td>
-                                        <td class="text-xs-right">{{ props.item.carbs }}</td>
-                                        <td class="text-xs-right">{{ props.item.protein }}</td>
-                                        <td class="text-xs-right">{{ props.item.iron }}</td>
-                                    </template>
-                                    <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                                        Your search for "{{ search }}" found no results.
-                                    </v-alert>
-                                </v-data-table>
-                            </v-card>
-                        </v-app>
-                    </div>
+
                     <div class="col pa-md-4 mx-lg-auto">
                         <v-card class="d-inline-block mx-auto">
                             <v-container>
