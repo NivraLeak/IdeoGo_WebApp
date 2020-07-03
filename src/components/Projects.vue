8aabce5badd40193196c5352eac28defef59e654
nivra <nivraleak@outlook.com>
2020-07-02 19:45:21 -0500

    agredando tarjetas de projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -103,6 +103,104 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="col pa-md-4 mx-lg-auto">
+                        <v-card class="d-inline-block mx-auto">
+                            <v-container>
+
+                                <v-row justify="space-between">
+
+                                    <v-col cols="auto">
+                                        <h5 class="text-center ">Ambiente</h5>
+                                        <v-img
+                                                height="200"
+                                                width="200"
+                                                src="https://cde.laprensa.e3.pe/ima/0/0/2/3/5/235222.jpg"
+                                        ></v-img>
+                                    </v-col>
+
+                                    <v-col
+                                            cols="auto"
+                                            class="text-center pl-0"
+                                    >
+                                        <v-row
+                                                class="flex-column ma-5 fill-height"
+                                                justify="center"
+                                        >
+                                            <h5 class="text-center ">Contaminacion</h5>
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Editar proyecto
+                                                </v-btn>
+                                            </v-col>
+
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Ver colaboradores
+                                                </v-btn>
+                                            </v-col>
+
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Ver solicitudes
+                                                </v-btn>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card>
+                    </div>
+
+                    <div class="col pa-md-4 mx-lg-auto">
+                        <v-card class="d-inline-block mx-auto">
+                            <v-container>
+
+                                <v-row justify="space-between">
+
+                                    <v-col cols="auto">
+                                        <h5 class="text-center ">Musica</h5>
+                                        <v-img
+                                                height="200"
+                                                width="200"
+                                                src="https://previews.123rf.com/images/abstract412/abstract4121711/abstract412171100008/89064788-dise%C3%B1o-de-carteles-de-m%C3%BAsica-con-cerebro-humano-con-cremallera-y-notas-musicales-coloridas-concepto-de-crea.jpg"
+                                        ></v-img>
+                                    </v-col>
+
+                                    <v-col
+                                            cols="auto"
+                                            class="text-center pl-0"
+                                    >
+                                        <v-row
+                                                class="flex-column ma-5 fill-height"
+                                                justify="center"
+                                        >
+                                            <h5 class="text-center ">Electronica</h5>
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Editar proyecto
+                                                </v-btn>
+                                            </v-col>
+
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Ver colaboradores
+                                                </v-btn>
+                                            </v-col>
+
+                                            <v-col >
+                                                <v-btn style="min-width:200px">
+                                                    Ver solicitudes
+                                                </v-btn>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card>
+                    </div>
+                </div>
+
 
             </div>
 
